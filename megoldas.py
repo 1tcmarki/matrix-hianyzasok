48ecb802cf8dd1aa3f148accb8cccab01184c996
1tccsinan <126462754+1tccsinan@users.noreply.github.com>
2023-03-09 10:06:26 +0100

    3.feladat kész

--- megoldas.py
+++ megoldas.py
@@ -22,4 +22,18 @@ print(f"# 1. feladat: {osszeg} óra hiányzás volt összesen?")
 
 
 # 3. Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt
-print(f"3. feladat: Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt")
+print(f"3. feladat: Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt?")
+
+van_otos_het=False
+for het in hianyzasok:
+    if sum(het) <= 5:
+        van_otos_het=True
+
+if van_otos_het:
+    print("Volt olyan hét, amikor ötnél kevesebb hiányzás volt.")
+else:
+    print("Nem volt olyan hét, amikor ötnél kevesebb hiányzás volt.")
+
+
+
+
